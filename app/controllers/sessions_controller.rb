2f29c66b39b26bbc4d01bee93c17596403f57d69
Zack <zackwilldev@gmail.com>
2022-01-04 08:47:00 -0500

    Fix redirect when login fails. Fix webpacker bug caused by stylesheets. Update gemfile. Update Yarnlock

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
   def create_fb
     if @user = User.find_or_create_by(uid: auth['uid']) do |u|
       u.name = auth['info']['name']
@@ -20,6 +21,7 @@ class SessionsController < ApplicationController
       flash[:alert] = @user.error.full_messages
     end
   end
+
   def create
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
@@ -28,9 +30,10 @@ class SessionsController < ApplicationController
       redirect_to user_path(@user)
     else
       flash[:message] = "Please try to login again"
-      render :new
+      redirect_to login_path
     end
   end
+
   def show
   end
 
